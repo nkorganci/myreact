72fbc9ce582b128084401a1884dd032e54cf7f63
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-07-03 12:10:33 -0500

    d

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
   return (
     <div className="App">
       <h1>Helo React</h1>
+      <p>React is good for portable</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+//Main library of react
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+//Adding app module
 import App from './App';
 
+//Rendering element: what you want to see on the screen or updating
+//Render gets two parameter: 1-What to do render
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<div>
+ <App />
+ <App />
+</div>{/* calling app module*/}
+   
+  ,
   document.getElementById('root')
 );
 
